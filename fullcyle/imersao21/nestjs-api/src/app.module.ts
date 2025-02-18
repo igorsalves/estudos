6d16cd74be91386105143a4d0d0e5508856bb560
igorsalves <igorsaj@hotmail.com>
2025-02-18 20:50:17 -0300

    feat: add Asset entity with create, findAll, and findOneBySymbol methods

--- fullcyle/imersao21/nestjs-api/src/app.module.ts
+++ fullcyle/imersao21/nestjs-api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
 
 @Module({
-  imports: [AssetsModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:root@localhost:27017/nestHomeBroker?directConnection=true&authSource=admin',
+    ),
+    AssetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- fullcyle/imersao21/nestjs-api/src/assets/assets.module.ts
+++ fullcyle/imersao21/nestjs-api/src/assets/assets.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AssetsService } from './assets.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AssetsController } from './assets.controller';
+import { AssetsService } from './assets.service';
+import { Asset, AssetSchema } from './entities/asset.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Asset.name,
+        schema: AssetSchema,
+      },
+    ]),
+  ],
   controllers: [AssetsController],
   providers: [AssetsService],
 })
