24657c3779facc555bc54fca396f64bac21e82fc
igorsalves <igorsaj@hotmail.com>
2025-07-26 14:48:15 -0300

    feat: add OrdersGateway for handling order creation via WebSocket

---
+++ fullcyle/imersao21/nestjs-api/src/orders/orders.gateway.ts
@@ -0,0 +1,30 @@
+import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OrderType } from './entities/order.entity';
+import { OrdersService } from './orders.service';
+
+@WebSocketGateway()
+export class OrdersGateway {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @SubscribeMessage('orders/create')
+  async handleMessage(
+    client: any,
+    payload: {
+      assetId: string;
+      walletId: string;
+      type: OrderType;
+      shares: number;
+      price: number;
+    },
+  ) {
+    const order = await this.ordersService.create({
+      assetId: payload.assetId,
+      walletId: payload.walletId,
+      type: payload.type,
+      shares: payload.shares,
+      price: payload.price,
+    });
+
+    return order;
+  }
+}
