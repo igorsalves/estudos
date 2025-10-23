a58dbb1602dec5338954385b15a96e7b2e455958
igorsalves <igorsaj@hotmail.com>
2025-10-23 13:29:45 -0300

    feat: add controller and presenter for daily asset prices

--- fullcyle/imersao21/nestjs-api/src/assets/assets.module.ts
+++ fullcyle/imersao21/nestjs-api/src/assets/assets.module.ts
@@ -4,6 +4,9 @@ import { AssetsController } from './assets.controller';
 import { AssetsService } from './assets.service';
 import { Asset, AssetSchema } from './entities/asset.entity';
 import { AssetsGateway } from './assets.gateway';
+import { AssetsDailiesController } from './asset-dailies.controller';
+import { AssetDailiesService } from './asset-dalies.service';
+import { AssetDaily, AssetDailySchema } from './entities/asset-daily.entity';
 
 @Module({
   imports: [
@@ -12,9 +15,11 @@ import { AssetsGateway } from './assets.gateway';
         name: Asset.name,
         schema: AssetSchema,
       },
+      { name: AssetDaily.name, schema: AssetDailySchema },
     ]),
   ],
-  controllers: [AssetsController],
-  providers: [AssetsService, AssetsGateway],
+  controllers: [AssetsController, AssetsDailiesController],
+  providers: [AssetsService, AssetsGateway, AssetDailiesService],
+  exports: [AssetsService],
 })
 export class AssetsModule {}
