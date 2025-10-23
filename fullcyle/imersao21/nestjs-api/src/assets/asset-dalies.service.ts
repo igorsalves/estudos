90065d588fa9505811b2944a9d95e1f0ccab9e18
igorsalves <igorsaj@hotmail.com>
2025-10-23 13:29:18 -0300

    feat: implement AssetDailiesService for daily asset price management

---
+++ fullcyle/imersao21/nestjs-api/src/assets/asset-dalies.service.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { InjectModel } from '@nestjs/mongoose';
+import { AssetDaily } from './entities/asset-daily.entity';
+import { Observable } from 'rxjs';
+import { Model } from 'mongoose';
+import { Asset } from './entities/asset.entity';
+
+export class AssetDailiesService {
+  constructor(
+    @InjectModel(AssetDaily.name)
+    private readonly assetDailySchema: Model<AssetDaily>,
+    @InjectModel(Asset.name) private readonly assetSchema: Model<Asset>,
+  ) {}
+
+  async findAll(symbol: string) {
+    const asset = await this.assetSchema.findOne({ symbol });
+    return this.assetDailySchema.find({ asset: asset!._id }).sort({ date: 1 });
+  }
+
+  async create(dto: { symbol: string; date: Date; price: number }) {
+    const asset = await this.assetSchema.findOne({ symbol: dto.symbol });
+    return this.assetDailySchema.create({
+      asset: asset!._id,
+      date: dto.date,
+      price: dto.price,
+    });
+  }
+
+  subscribeCreatedEvents(): Observable<AssetDaily & { asset: Asset }> {
+    return new Observable((observer) => {
+      this.assetDailySchema
+        .watch(
+          [
+            {
+              $match: {
+                operationType: 'insert',
+              },
+            },
+          ],
+          { fullDocument: 'updateLookup' },
+        )
+        .on('change', async (data) => {
+          const assetDaily = await this.assetDailySchema
+            .findById(data.fullDocument._id)
+            .populate('asset');
+          observer.next(assetDaily! as AssetDaily & { asset: Asset });
+        });
+    });
+  }
+}
