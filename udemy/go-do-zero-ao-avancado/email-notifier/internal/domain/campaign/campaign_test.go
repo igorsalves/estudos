18c5f0e9bc525560aa08db5d656b3d7bc53ee98f
igorsalves <igorsaj@hotmail.com>
2025-12-26 14:43:17 -0300

    test: add tests for NewCampaign function to validate ID and CreatedOn

--- udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/campaign_test.go
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/campaign_test.go
@@ -2,11 +2,12 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	name := "Campaign x"
 	content := "This is the content of Campaign x."
 	contacts := []string{"contact1@example.com", "contact2@example.com"}
@@ -14,8 +15,31 @@ func TestNewCampaign(t *testing.T) {
 	campaign := NewCampaign(name, content, contacts)
 
 	assert := assert.New(t)
-	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_NewCampaign_IDIsNotNil(t *testing.T) {
+	name := "Campaign x"
+	content := "This is the content of Campaign x."
+	contacts := []string{"contact1@example.com", "contact2@example.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert := assert.New(t)
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedOnIsNotNil(t *testing.T) {
+	name := "Campaign x"
+	content := "This is the content of Campaign x."
+	contacts := []string{"contact1@example.com", "contact2@example.com"}
+	now := time.Now().Add(-1 * time.Minute)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert := assert.New(t)
+	assert.NotNil(campaign.CreatedOn)
+	assert.True(campaign.CreatedOn.After(now))
 }
