4ab18b8815415c99f58b1d931fd425df31c6b457
igorsalves <igorsaj@hotmail.com>
2025-12-27 11:43:25 -0300

    refactor: enhance validation for campaign creation

--- udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/campaign.go
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/campaign.go
@@ -1,47 +1,44 @@
 package campaign
 
 import (
-	"errors"
 	"time"
 
+	internalerrors "github.com/igorsalves/estudos/tree/main/udemy/go-do-zero-ao-avancado/email-notifier/internal/internal-errors"
 	"github.com/rs/xid"
 )
 
 type Contacts struct {
-	Email string
+	Email string `validate:"email"`
 }
 
 type Campaign struct {
-	ID        string
-	Name      string
-	CreatedOn time.Time
-	Content   string
-	Contacts  []Contacts
+	ID        string     `validate:"required"`
+	Name      string     `validate:"min=5,max=24"`
+	CreatedOn time.Time  `validate:"required"`
+	Content   string     `validate:"min=5,max=1024"`
+	Contacts  []Contacts `validate:"min=1,dive"`
 }
 
 func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
-	if name == "" {
-		return nil, errors.New("name is required")
-	}
-
-	if content == "" {
-		return nil, errors.New("content is required")
-	}
-
-	if len(emails) == 0 {
-		return nil, errors.New("at least one contact is required")
-	}
-
 	contacts := make([]Contacts, len(emails))
+
 	for index, email := range emails {
 		contacts[index] = Contacts{Email: email}
 	}
 
-	return &Campaign{
+	campagin := &Campaign{
 		ID:        xid.New().String(),
 		Name:      name,
 		CreatedOn: time.Now(),
 		Content:   content,
 		Contacts:  contacts,
-	}, nil
+	}
+
+	err := internalerrors.ValidateStruct(campagin)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return campagin, nil
 }
