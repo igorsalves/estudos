bce6e5c660f5a1ae95965cb6fc3f5196ae0b1d1a
igorsalves <igorsaj@hotmail.com>
2025-12-28 15:55:40 -0300

    feat: campaign status starts with Pending

--- udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/campaign.go
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/campaign.go
@@ -7,38 +7,46 @@ import (
 	"github.com/rs/xid"
 )
 
-type Contacts struct {
+const (
+	Pending string = "Pending"
+	Started string = "Started"
+	Done    string = "Done"
+)
+
+type Contact struct {
 	Email string `validate:"email"`
 }
 
 type Campaign struct {
-	ID        string     `validate:"required"`
-	Name      string     `validate:"min=5,max=24"`
-	CreatedOn time.Time  `validate:"required"`
-	Content   string     `validate:"min=5,max=1024"`
-	Contacts  []Contacts `validate:"min=1,dive"`
+	ID        string    `validate:"required"`
+	Name      string    `validate:"min=5,max=24"`
+	CreatedOn time.Time `validate:"required"`
+	Content   string    `validate:"min=5,max=1024"`
+	Contacts  []Contact `validate:"min=1,dive"`
+	Status    string
 }
 
 func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
-	contacts := make([]Contacts, len(emails))
+	contacts := make([]Contact, len(emails))
 
 	for index, email := range emails {
-		contacts[index] = Contacts{Email: email}
+		contacts[index] = Contact{Email: email}
 	}
 
-	campagin := &Campaign{
+	campaign := &Campaign{
 		ID:        xid.New().String(),
 		Name:      name,
 		CreatedOn: time.Now(),
 		Content:   content,
 		Contacts:  contacts,
+		Status:    Pending,
 	}
 
-	err := internalerrors.ValidateStruct(campagin)
+	err := internalerrors.ValidateStruct(campaign)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return campagin, nil
+	return campaign, nil
 }
