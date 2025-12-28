594ebe095fc20544aa0c9930202f104efdbfd769
igorsalves <igorsaj@hotmail.com>
2025-12-28 11:16:27 -0300

    feat: add CampaignGet and CampaignPost handlers

---
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/endpoints/campaigns_post.go
@@ -0,0 +1,17 @@
+package endpoints
+
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+	"github.com/igorsalves/estudos/tree/main/udemy/go-do-zero-ao-avancado/email-notifier/internal/contract"
+)
+
+func (h *Handler) CampaignPost(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+	var request contract.NewCampaign
+	render.DecodeJSON(r.Body, &request)
+
+	id, err := h.CampaignService.Create(request)
+
+	return map[string]string{"id": id}, 201, err
+}
