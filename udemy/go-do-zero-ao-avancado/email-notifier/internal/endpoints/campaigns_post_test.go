d1f2ea96ae210ed6369a092869ba1a9a612fb173
igorsalves <igorsaj@hotmail.com>
2025-12-31 15:18:39 -0300

    test: add CampaignServiceMock for campaign testing

--- udemy/go-do-zero-ao-avancado/email-notifier/internal/endpoints/campaigns_post_test.go
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/endpoints/campaigns_post_test.go
@@ -9,31 +9,19 @@ import (
 	"testing"
 
 	"github.com/igorsalves/estudos/tree/main/udemy/go-do-zero-ao-avancado/email-notifier/internal/contract"
+	internalmock "github.com/igorsalves/estudos/tree/main/udemy/go-do-zero-ao-avancado/email-notifier/internal/test/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type serviceMock struct {
-	mock.Mock
-}
-
-func (r *serviceMock) Create(newCampaign contract.NewCampaign) (string, error) {
-	args := r.Called(newCampaign)
-	return args.String(0), args.Error(1)
-}
-func (r *serviceMock) GetBy(id string) (*contract.CampaignResponse, error) {
-	args := r.Called(id)
-	return args.Get(0).(*contract.CampaignResponse), args.Error(1)
-}
-
-func Test_CampaignsPost_should_save_new_camapaign(t *testing.T) {
+func Test_CampaignsPost_should_save_new_campaign(t *testing.T) {
 	assert := assert.New(t)
 	body := contract.NewCampaign{
 		Name:    "teste",
 		Content: "Hi everyone",
 		Emails:  []string{"teste@teste.com"},
 	}
-	service := new(serviceMock)
+	service := new(internalmock.CampaignServiceMock)
 	service.On("Create", mock.MatchedBy(func(request contract.NewCampaign) bool {
 		if request.Name == body.Name && request.Content == body.Content {
 			return true
@@ -61,7 +49,7 @@ func Test_CampaignsPost_should_inform_error_when_exist(t *testing.T) {
 		Content: "Hi everyone",
 		Emails:  []string{"teste@teste.com"},
 	}
-	service := new(serviceMock)
+	service := new(internalmock.CampaignServiceMock)
 	service.On("Create", mock.Anything).Return("", fmt.Errorf("error"))
 	handler := Handler{CampaignService: service}
 
---
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/test/mock/campaign_service_mock.go
@@ -0,0 +1,23 @@
+package mock
+
+import (
+	"github.com/igorsalves/estudos/tree/main/udemy/go-do-zero-ao-avancado/email-notifier/internal/contract"
+	"github.com/stretchr/testify/mock"
+)
+
+type CampaignServiceMock struct {
+	mock.Mock
+}
+
+func (r *CampaignServiceMock) Create(newCampaign contract.NewCampaign) (string, error) {
+	args := r.Called(newCampaign)
+	return args.String(0), args.Error(1)
+}
+
+func (r *CampaignServiceMock) GetBy(id string) (*contract.CampaignResponse, error) {
+	args := r.Called(id)
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*contract.CampaignResponse), args.Error(1)
+}
