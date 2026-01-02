c0ba17c22412da1eda2f63362c3f577e7f24db64
igorsalves <igorsaj@hotmail.com>
2026-01-02 16:39:16 -0300

    feat: implement SendMail function for campaign email notifications

---
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/infrastructure/mail/send_mail.go
@@ -0,0 +1,28 @@
+package mail
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/igorsalves/estudos/tree/main/udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign"
+	"gopkg.in/gomail.v2"
+)
+
+func SendMail(campaign *campaign.Campaign) error {
+	fmt.Println("Sending mail...")
+
+	d := gomail.NewDialer(os.Getenv("EMAIL_SMTP"), 587, os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"))
+
+	var emails []string
+	for _, contact := range campaign.Contacts {
+		emails = append(emails, contact.Email)
+	}
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", os.Getenv("EMAIL_USER"))
+	m.SetHeader("To", emails...)
+	m.SetHeader("Subject", campaign.Name)
+	m.SetBody("text/plain", campaign.Content)
+
+	return d.DialAndSend(m)
+}
