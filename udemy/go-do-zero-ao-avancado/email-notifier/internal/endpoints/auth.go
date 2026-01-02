ba42d8e70d54c7fe33e7e2be8eb80fe486ab32e9
igorsalves <igorsaj@hotmail.com>
2026-01-02 11:56:14 -0300

    feat: extract email from token and include in campaign creation

--- udemy/go-do-zero-ao-avancado/email-notifier/internal/endpoints/auth.go
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/endpoints/auth.go
@@ -1,24 +1,26 @@
 package endpoints
 
 import (
+	"context"
 	"net/http"
 	"strings"
 
 	oidc "github.com/coreos/go-oidc/v3/oidc"
 	"github.com/go-chi/render"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
+		tokenString := r.Header.Get("Authorization")
 
-		if token == "" {
+		if tokenString == "" {
 			render.Status(r, http.StatusUnauthorized)
 			render.JSON(w, r, map[string]string{"error": "request does not contain an authorization header"})
 			return
 		}
 
-		token = strings.Replace(token, "Bearer ", "", 1)
+		tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
 		provider, err := oidc.NewProvider(r.Context(), "http://localhost:8080/realms/provider")
 
 		if err != nil {
@@ -28,7 +30,7 @@ func Auth(next http.Handler) http.Handler {
 		}
 
 		verifier := provider.Verifier(&oidc.Config{ClientID: "emailn"})
-		_, err = verifier.Verify(r.Context(), token)
+		_, err = verifier.Verify(r.Context(), tokenString)
 
 		if err != nil {
 			render.Status(r, http.StatusUnauthorized)
@@ -36,6 +38,11 @@ func Auth(next http.Handler) http.Handler {
 			return
 		}
 
-		next.ServeHTTP(w, r)
+		token, _ := jwt.Parse(tokenString, nil)
+		claims := token.Claims.(jwt.MapClaims)
+		email := claims["email"].(string)
+
+		ctx := context.WithValue(r.Context(), "email", email)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
