b47b93447c0bc7a9b903876f4889a148b7146e2c
igorsalves <igorsaj@hotmail.com>
2026-01-02 17:22:09 -0300

    refactor: consolidate campaign management methods and improve error handling

--- udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/service.go
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/service.go
@@ -10,28 +10,22 @@ import (
 type Service interface {
 	Create(newCampaign contract.NewCampaign) (string, error)
 	GetBy(id string) (*contract.CampaignResponse, error)
-	Cancel(id string) error
 	Delete(id string) error
+	Start(id string) error
 }
 
 type ServiceImp struct {
 	Repository Repository
+	SendMail   func(campaign *Campaign) error
 }
 
 func (s *ServiceImp) Create(newCampaign contract.NewCampaign) (string, error) {
-	campaign, err := NewCampaign(
-		newCampaign.Name,
-		newCampaign.Content,
-		newCampaign.Emails,
-		newCampaign.CreatedBy,
-	)
-
+	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails, newCampaign.CreatedBy)
 	if err != nil {
 		return "", err
 	}
 
 	err = s.Repository.Create(campaign)
-
 	if err != nil {
 		return "", internalerrors.ErrInternal
 	}
@@ -40,6 +34,7 @@ func (s *ServiceImp) Create(newCampaign contract.NewCampaign) (string, error) {
 }
 
 func (s *ServiceImp) GetBy(id string) (*contract.CampaignResponse, error) {
+
 	campaign, err := s.Repository.GetBy(id)
 
 	if err != nil {
@@ -56,20 +51,16 @@ func (s *ServiceImp) GetBy(id string) (*contract.CampaignResponse, error) {
 	}, nil
 }
 
-func (s *ServiceImp) Cancel(id string) error {
+func (s *ServiceImp) Delete(id string) error {
 
-	campaign, err := s.Repository.GetBy(id)
+	campaignSaved, err := s.getAndValidateStatusIsPending(id)
 
 	if err != nil {
-		return internalerrors.ProcessErrorToReturn(err)
+		return err
 	}
 
-	if campaign.Status != Pending {
-		return errors.New("Campaign status invalid")
-	}
-
-	campaign.Cancel()
-	err = s.Repository.Update(campaign)
+	campaignSaved.Delete()
+	err = s.Repository.Delete(campaignSaved)
 	if err != nil {
 		return internalerrors.ErrInternal
 	}
@@ -77,22 +68,45 @@ func (s *ServiceImp) Cancel(id string) error {
 	return nil
 }
 
-func (s *ServiceImp) Delete(id string) error {
-	campaign, err := s.Repository.GetBy(id)
+func (s *ServiceImp) SendEmailAndUpdateStatus(campaignSaved *Campaign) {
+	err := s.SendMail(campaignSaved)
+	if err != nil {
+		campaignSaved.Fail()
+	} else {
+		campaignSaved.Done()
+	}
+	s.Repository.Update(campaignSaved)
+}
+
+func (s *ServiceImp) Start(id string) error {
+
+	campaignSaved, err := s.getAndValidateStatusIsPending(id)
 
 	if err != nil {
-		return internalerrors.ProcessErrorToReturn(err)
+		return err
 	}
 
-	if campaign.Status != Pending {
-		return errors.New("Campaign status invalid")
-	}
+	go s.SendEmailAndUpdateStatus(campaignSaved)
 
-	campaign.Delete()
-	err = s.Repository.Delete(campaign)
+	campaignSaved.Started()
+	err = s.Repository.Update(campaignSaved)
 	if err != nil {
 		return internalerrors.ErrInternal
 	}
 
 	return nil
+}
+
+func (s *ServiceImp) getAndValidateStatusIsPending(id string) (*Campaign, error) {
+	campaign, err := s.Repository.GetBy(id)
+
+	if err != nil {
+		return nil, internalerrors.ProcessErrorToReturn(err)
+	}
+
+	if campaign.Status != Pending {
+		return nil, errors.New("Campaign status invalid")
+	}
+
+	return campaign, nil
 }
