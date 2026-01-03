aa2c93937b9ff70f7acd0480af1904bc00fc2e6a
igorsalves <igorsaj@hotmail.com>
2026-01-03 10:37:15 -0300

    test: add unit tests for campaign delete and start endpoints

--- udemy/go-do-zero-ao-avancado/email-notifier/internal/endpoints/campaigns_get_by_id_test.go
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/endpoints/campaigns_get_by_id_test.go
@@ -2,49 +2,40 @@ package endpoints
 
 import (
 	"errors"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/igorsalves/estudos/tree/main/udemy/go-do-zero-ao-avancado/email-notifier/internal/contract"
-	internalmock "github.com/igorsalves/estudos/tree/main/udemy/go-do-zero-ao-avancado/email-notifier/internal/test/internal-mock"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-func Test_CampaignsGetById_should_return_campaign(t *testing.T) {
-	assert := assert.New(t)
-	campaignResponse := contract.CampaignResponse{
-		ID:      "321",
-		Name:    "teste",
-		Content: "Hi everyone",
+func Test_CampaignsGetById_Campaign(t *testing.T) {
+	setup()
+	campaignId := "343"
+	campaign := contract.CampaignResponse{
+		ID:      campaignId,
+		Name:    "Test",
+		Content: "Hi!",
 		Status:  "Pending",
 	}
-	service := new(internalmock.CampaignServiceMock)
-	service.On("GetBy", mock.Anything).Return(&campaignResponse, nil)
-	handler := Handler{CampaignService: service}
-	req, _ := http.NewRequest("GET", "/321", nil)
-	rr := httptest.NewRecorder()
+	service.On("GetBy", mock.Anything).Return(&campaign, nil)
+	req, rr := newHttpTest("GET", "/", nil)
+	req = addParameter(req, "id", campaignId)
 
 	response, status, _ := handler.CampaignGetById(rr, req)
 
-	assert.Equal(200, status)
-	assert.Equal(campaignResponse.ID, response.(*contract.CampaignResponse).ID)
-	assert.Equal(campaignResponse.Name, response.(*contract.CampaignResponse).Name)
+	assert.Equal(t, 200, status)
+	assert.Equal(t, campaign.ID, response.(*contract.CampaignResponse).ID)
+	assert.Equal(t, campaign.Name, response.(*contract.CampaignResponse).Name)
 }
 
-func Test_CampaignsGetById_should_return_error_when_something_wrong(t *testing.T) {
-	assert := assert.New(t)
-	service := new(internalmock.CampaignServiceMock)
+func Test_CampaignsGetById_Err(t *testing.T) {
+	setup()
 	errExpected := errors.New("something wrong")
 	service.On("GetBy", mock.Anything).Return(nil, errExpected)
-	handler := Handler{CampaignService: service}
-	req, _ := http.NewRequest("GET", "/", nil)
-	rr := httptest.NewRecorder()
+	req, rr := newHttpTest("GET", "/", nil)
 
 	_, _, errReturned := handler.CampaignGetById(rr, req)
 
-	assert.Equal(errExpected.Error(), errReturned.Error())
-
+	assert.Equal(t, errExpected.Error(), errReturned.Error())
 }
