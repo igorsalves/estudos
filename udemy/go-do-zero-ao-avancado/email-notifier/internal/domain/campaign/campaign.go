bd32e58a8807bed7a9096ad98ece32410d4f73ce
igorsalves <igorsaj@hotmail.com>
2026-01-03 16:09:39 -0300

    fix: update UpdatedOn field on campaign status changes

--- udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/campaign.go
+++ udemy/go-do-zero-ao-avancado/email-notifier/internal/domain/campaign/campaign.go
@@ -26,7 +26,7 @@ type Campaign struct {
 	ID        string    `validate:"required" gorm:"size:50;not null"`
 	Name      string    `validate:"min=5,max=24" gorm:"size:100;not null"`
 	CreatedOn time.Time `validate:"required" gorm:"not null"`
-	UpdatedOn time.Time
+	UpdatedOn time.Time `gorm:"autoUpdateTime"`
 	Content   string    `validate:"min=5,max=1024" gorm:"size:1024;not null"`
 	Contacts  []Contact `validate:"min=1,dive"`
 	Status    string    `gorm:"size:20;not null"`
@@ -35,27 +35,22 @@ type Campaign struct {
 
 func (c *Campaign) Done() {
 	c.Status = Done
-	c.UpdatedOn = time.Now()
 }
 
 func (c *Campaign) Cancel() {
 	c.Status = Canceled
-	c.UpdatedOn = time.Now()
 }
 
 func (c *Campaign) Delete() {
 	c.Status = Deleted
-	c.UpdatedOn = time.Now()
 }
 
 func (c *Campaign) Fail() {
 	c.Status = Fail
-	c.UpdatedOn = time.Now()
 }
 
 func (c *Campaign) Started() {
 	c.Status = Started
-	c.UpdatedOn = time.Now()
 }
 
 func NewCampaign(name string, content string, emails []string, createdBy string) (*Campaign, error) {
